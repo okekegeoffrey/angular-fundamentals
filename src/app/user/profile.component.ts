87f317f568afc63aef32bd032a2e9a662d1b08b4
geoffreyokeke <cokekegeoffrey@gmail.com>
2021-12-26 21:55:59 +0100

    finished learning how to validate reactive form

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,14 +1,33 @@
 import { Component, OnInit } from '@angular/core'
-import { FormControl, FormGroup } from '@angular/forms'
+import { FormControl, FormGroup, Validators } from '@angular/forms'
 import { Router } from '@angular/router';
 import { AuthService } from './auth.service';
 
 @Component({
   templateUrl: "./profile.component.html",
+  styles: [`
+    em {
+      float: right;
+      color: #e05c65;
+      padding-left: 10px;
+    }
+
+    .error input {
+      background-color: #e3c3c5;
+    }
+
+    /* styling the placeholder for each browser */
+    .error ::-webkit-input-placeholder {color: #999;}
+    .error ::-moz-placeholder {color: #999;}
+    .error :-moz-placeholder {color: #999;}
+    .error :ms-input-placeholder{color: #999}
+  `]
 })
 export class ProfileComponent implements OnInit {
 
   profileForm!:FormGroup;
+  private firstName!:FormControl;
+  private lastName!:FormControl;
 
   constructor(private authService: AuthService, private router: Router) {
 
@@ -16,21 +35,33 @@ export class ProfileComponent implements OnInit {
 
   ngOnInit() {
     // form control for the inputs
-    let firstName = new FormControl(this.authService.currentUser.firstName);
-    let lastName = new FormControl(this.authService.currentUser.lastName);
+    this.firstName = new FormControl(this.authService.currentUser.firstName, Validators.required);
+    this.lastName = new FormControl(this.authService.currentUser.lastName, Validators.required);
 
     // add these controls to a form: use formgroup for that
     this.profileForm = new FormGroup({
       // set properties for each of the controls
-      firstName: firstName,
-      lastName: lastName
+      firstName: this.firstName,
+      lastName: this.lastName
     })
 
   }
 
   saveProfile(formValues:any) {
-    this.authService.updateCurrentUser(formValues.firstName, formValues.lastName);
+    // navigate if form is valid
+    if (this.profileForm.valid) {
+      this.authService.updateCurrentUser(formValues.firstName, formValues.lastName);
     this.router.navigate(['events']);
+    }
+
+  }
+
+  validateFirstName() {
+    return this.firstName.valid || this.firstName.touched
+  }
+
+  validateLastName() {
+    return this.lastName.valid || this.lastName.touched
   }
 
   cancel() {
